fcace8a5dde7864ea1c49eaf3d1cb3c96706d9ec
SethKaminski <sethgabe@yahoo.com>
2020-10-03 14:44:07 -0400

    Reworked cctvController #4

--- Assets/0-Scripts/cctvController.cs
+++ Assets/0-Scripts/cctvController.cs
@@ -1,41 +1,34 @@
 ﻿using UnityEngine;
 
-// CCTV CONTROLLER CLASS
 public class cctvController : MonoBehaviour {
-    // PUBLIC INSTANCE VARIABLES
-    public int strikeAdd = 1;
+    #region Variables and Properties
+    [SerializeField] private int strikeAdd = 1;
+    [SerializeField] private GameController gameController;
 
-    // PRIVATE INSTANCE VARIABLES
-    //private GameObject player;
-    private GameController _gameController;
+    private Transform cachedTransform;
+    #endregion Variables and Properties
 
-    void Awake() {
-        GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
-        if (gameControllerObject != null) {
-            this._gameController = gameControllerObject.GetComponent<GameController>();
-        } else {
-            Debug.Log("cctv Controller Class Cannot find 'GameController' script");
-        }
+    private void Awake() {
+        cachedTransform = transform;
     }
 
-    void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "Player") {
-            Vector3 relPlayPos = other.transform.position - transform.position;
-            RaycastHit hit;
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            Vector3 relPlayPos = other.transform.position - cachedTransform.position;
 
-            if (Physics.Raycast(transform.position, relPlayPos, out hit)) {
-                if (hit.collider.gameObject.tag == "Player") {
-                    Debug.Log("HEllO");
-                    this._gameController.AddStrikes(strikeAdd);
+            if (Physics.Raycast(cachedTransform.position, relPlayPos, out RaycastHit hit)) {
+                if (hit.collider.CompareTag("Player")) {
+                    Debug.Log("cctv sees player");
+                    gameController.AddStrikes(strikeAdd);
                     //Player.'Number of people looking at look'++;
                 }
             }
         }
     }
 
-    void OnTriggerExit(Collider other) {
-        if (other.gameObject.tag == "Player") {
+    /*private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) {
             //Player.'Number of people looking at look'--;
         }
-    }
+    }*/
 }
